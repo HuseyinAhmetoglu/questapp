982a64fd53f01363417e3aae17907fbcc3f1b8d1
Huseyin Ahmetoglu <huseyinahmetoglu@artuklu.edu.tr>
2023-02-25 17:12:25 +0300

    Commit

---
+++ src/main/java/com/project/questapp/services/UserDetailsServiceImpl.java
@@ -0,0 +1,32 @@
+package com.project.questapp.services;
+
+import com.project.questapp.entities.User;
+import com.project.questapp.repos.UserRepository;
+import com.project.questapp.security.JwtUserDetails;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    private UserRepository userRepository;
+
+    public UserDetailsServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUserName(username);
+        return JwtUserDetails.create(user);
+    }
+
+    public UserDetails loadUserById(Long userid) {
+        User user = userRepository.findById(userid).get();
+        return JwtUserDetails.create(user);
+    }
+
+
+}
